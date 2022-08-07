b36299a443e2321931365d039d5ac7eef16f3bec
Gary Dorman <garydorman619@gmail.com>
2022-08-07 12:48:35 -0400

    Finished Project

---
+++ script.js
@@ -0,0 +1,54 @@
+const hour_value = document.querySelector(".hour_value");
+
+const minute_value = document.querySelector(".minute_value");
+
+const second_value = document.querySelector(".second_value");
+
+const ampm_value = document.querySelector(".ampm_value");
+
+
+function updateClock() {
+
+    let today = new Date();
+
+    let current_hour = today.getHours();
+    let current_minute = today.getMinutes();
+    let current_second = today.getSeconds();
+
+    if (current_hour === 0){
+        hour_value.innerText = '12';
+    }
+    else if (current_hour < 10){
+        hour_value.innerText = '0' + current_hour;
+    }
+    else{
+        hour_value.innerText = current_hour;
+    }
+
+    if (current_minute < 10){
+        minute_value.innerText = '0' + current_minute;
+    }
+    else{
+        minute_value.innerText = current_minute;
+    }
+
+    if (current_second < 10){
+        second_value.innerText = '0' + current_second;
+    }
+    else{
+        second_value.innerText = current_second;
+    }
+
+    if (current_hour < 12){
+        ampm_value.innerText = 'AM';
+    }
+    else{
+        ampm_value.innerText = 'PM'
+    }
+
+    
+};
+
+updateClock()
+
+setInterval(updateClock, 1000); // Repeat updateClock Function every 1 second
